1bac10fb3be448c653710798446db7e8b69e72d4
Joseph-Kimuhu <joseph.kimuhu@student.moringaschool.com>
2026-01-14 07:12:10 +0300

    completion

--- server/app.py
+++ server/app.py
@@ -12,6 +12,7 @@ except ImportError:
 app = Flask(__name__)
 # Use absolute path to the database in server directory
 db_path = os.path.join(os.path.dirname(__file__), 'instance', 'app.db')
+os.makedirs(os.path.dirname(db_path), exist_ok=True)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
